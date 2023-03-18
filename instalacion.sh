2776d2728186e3eb6f050106947e4e5dba0efd83
Juanjosesanchezgil <juanjosesanchezgil@gmail.com>
2023-03-18 21:22:37 +0100

    Add files via upload

--- instalacion.sh
+++ instalacion.sh
@@ -5,25 +5,25 @@ echo -------------------------
 echo Sistema de particiones
 echo -------------------------
 
-mkfs.ext4 /dev/sda3
-mkfs.ext4 /dev/sda4
-mkfs.fat -F 32 /dev/sda1
-mkswap /dev/sda2
+mkfs.ext4 /dev/nvme0n1p3
+mkfs.ext4 /dev/nvme0n1p4
+mkfs.fat -F 32 /dev/nvme0n1p1
+mkswap /dev/nvme0n1p2
 
 echo -------------------------
 echo Montando particiones
 echo -------------------------
 
-mount /dev/sda3 /mnt
-mount --mkdir /dev/sda1 /mnt/boot
-mount --mkdir /dev/sda4 /mnt/home
-swapon /dev/sda2
+mount /dev/nvme0n1p3 /mnt
+mount --mkdir /dev/nvme0n1p1 /mnt/boot
+mount --mkdir /dev/nvme0n1p4 /mnt/home
+swapon /dev/nvme0n1p2
 
 echo -------------------------
 echo Instalando paquetes
 echo -------------------------
 
-pacstrap -K /mnt base base-devel linux linux-firmware nano intel-ucode xorg i3-wm pulseaudio networkmanager grub efibootmgr alacritty dbeaver git i3status intellij-idea-community-edition lightdm lightdm-gtk-greater network-manager-applet nitrogen nm-connection-editor papirus-icon-theme picom rofi seahorse vlc unzip
+pacstrap -K /mnt base base-devel linux linux-firmware nano intel-ucode 
 
 echo -------------------------
 echo Instalando el sistema
@@ -32,6 +32,9 @@ echo -------------------------
 genfstab -U /mnt >> /mnt/etc/fstab
 
 arch-chroot /mnt
+
+pacman -Syu
+pacman -S xorg i3-wm pulseaudio networkmanager grub efibootmgr alacritty dbeaver git i3status intellij-idea-community-edition lightdm lightdm-gtk-greater network-manager-applet nitrogen nm-connection-editor papirus-icon-theme picom rofi seahorse vlc unzip
 
 echo -------------------------
 echo Opciones de localizacion
@@ -63,7 +66,7 @@ echo -------------------------
 
 passwd juanj
 
-usermod -aG wheel,audio,video,storage
+usermod -aG wheel,audio,video,storage juanj
 
 echo LANG=es_ES.UTF-8 >> /etc/locale.conf
 echo KEYMAP=es >> /etc/vconsole.conf
