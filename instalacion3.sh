e5e1f546a5ac0e0e624bf02e97d7c84a79b479e6
Juanjosesanchezgil <juanjosesanchezgil@gmail.com>
2023-04-01 19:41:46 +0200

    Update instalacion3.sh

--- instalacion3.sh
+++ instalacion3.sh
@@ -5,6 +5,8 @@ echo -------------------------
 echo YAY y AUR
 echo -------------------------
 
-echo "hola"
+pacman -U yay-11.3.2-1-x86_64.pkg.tar.zst --noconfirm
+
+yay -S google-chrome visual-studio-code-bin --noconfirm
 
 read -rsp $'Press enter to continue...\n'
