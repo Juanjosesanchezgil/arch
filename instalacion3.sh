83a1548fbfe9f6b69360d6ac684119449b5f8bec
Juanjosesanchezgil <juanjosesanchezgil@gmail.com>
2023-05-15 12:57:22 +0200

    Update instalacion3.sh

--- instalacion3.sh
+++ instalacion3.sh
@@ -15,5 +15,7 @@ sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/too
 
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 
-sed -i 's/plugins=(git)/plugins=(\n \git\n \zsh-autosuggestions\n \)/g' .zshrc
+git clone git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+sed -i 's/plugins=(git)/plugins=(\n \git\n \zsh-autosuggestions\n \zsh-syntax-highlighting\n \)/g' .zshrc
 
