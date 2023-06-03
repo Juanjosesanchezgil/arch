3b9fa5d211e08b966c78def3bbd5ce020ca50098
Juanjosesanchezgil <juanjosesanchezgil@gmail.com>
2023-06-03 11:15:39 +0200

    Update postinstalacion.sh

--- postinstalacion.sh
+++ postinstalacion.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # -*- ENCODING: UTF-8 -*-
 
+cd ~
+git clone https://github.com/jluttine/rofi-power-menu.git .rofi-power-menu
+
+git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si
